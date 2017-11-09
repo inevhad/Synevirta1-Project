b53b1dc16d0f069cf8ff90a9603aea22d0136728
inev <ine.sena@gmail.com>
2017-11-09 22:25:54 +0700

    update main activity di MainVer2Activity.java dan activity_main_ver2.xml (input data puskesmas dlm alert window)

--- app/src/main/java/com/procodecg/codingmom/ehealth/main/MainVer2Activity.java
+++ app/src/main/java/com/procodecg/codingmom/ehealth/main/MainVer2Activity.java
@@ -1,12 +1,11 @@
 package com.procodecg.codingmom.ehealth.main;
 
-import android.content.ContentValues;
-import android.content.DialogInterface;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -25,10 +24,15 @@ public class MainVer2Activity extends AppCompatActivity {
 
 
     private SetConfigVer2 setConfig;
-    private EditText idPuskesmas,namaPuskesmas;
-    private Button simpan, hapus;
-    private TextView txtIdPuskesmas;
-    private TextView txtNamaPuskesmas;
+//    private EditText idPuskesmas,namaPuskesmas;
+//    private Button simpan, hapus;
+//    private TextView txtIdPuskesmas;
+//    private TextView txtNamaPuskesmas;
+    ImageButton DialogPuskesmas;
+    Button Show;
+    TextView IDPuskesmas, NamaPuskesmas;
+    Dialog ThisDialog;
+
 
 
     @Override
@@ -37,56 +41,84 @@ public class MainVer2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main_ver2);
 
         copyDBEhealth();
-        getHPCdata();
 
+        DialogPuskesmas = (ImageButton) findViewById(R.id.btnSetting);
+        Show = (Button) findViewById(R.id.btnShow);
+        IDPuskesmas = (TextView) findViewById(R.id.idPuskesmas);
+        NamaPuskesmas = (TextView) findViewById(R.id.namaPuskesmas);
 
-        txtIdPuskesmas = (TextView) findViewById(R.id.txt_idPuskesmas);
-        txtIdPuskesmas.setText("12121121");
-
-        txtNamaPuskesmas = (TextView) findViewById(R.id.txt_namaPuskesmas);
-        txtNamaPuskesmas.setText("Sarijadi");
-
-        ImageButton settingButton = (ImageButton) findViewById(R.id.btnSetting);
-        settingButton.setOnClickListener(new View.OnClickListener(){
+        DialogPuskesmas.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainVer2Activity.this);
-                View mView = getLayoutInflater().inflate(R.layout.dialog_puskesmas,null);
+            public void onClick(View v) {
+                ThisDialog = new Dialog(MainVer2Activity.this);
+                ThisDialog.setContentView(R.layout.dialog_puskesmas);
 
-                setConfig = new SetConfigVer2(getApplicationContext() );
-                idPuskesmas = (EditText) findViewById(R.id.idPuskesmas);
-                namaPuskesmas= (EditText) findViewById(R.id.namaPuskesmas);
-                simpan = (Button) mView.findViewById(R.id.btnSimpan);
+                final EditText WriteId = (EditText)ThisDialog.findViewById(R.id.idPuskesmas);
+                final EditText WriteNama = (EditText)ThisDialog.findViewById(R.id.namaPuskesmas);
+                Button Simpan = (Button)ThisDialog.findViewById(R.id.btnSimpan);
 
-//                HashMap<String, String> setTexting = setConfig.getDetail();
-//                idPuskesmas.setText(setTexting.get(setConfig.KEY_IDPUSKES));
-//                namaPuskesmas.setText(setTexting.get(setConfig.KEY_NAMAPUSKES));
+                WriteId.setEnabled(true);
+                WriteNama.setEnabled(true);
+                Simpan.setEnabled(true);
 
-                simpan.setOnClickListener(new View.OnClickListener() {
+                Simpan.setOnClickListener(new View.OnClickListener(){
                     @Override
-                    public void onClick(View view){
-//                        setConfig.createSetConfig(
-//                                idPuskesmas.getText().toString(),
-//                                namaPuskesmas.getText().toString()
-//                        );
+                    public void onClick(View v){
+                        SharedPrefesSimpan(WriteId.getText().toString());
+                        SharedPrefesSimpan(WriteNama.getText().toString());
+                        ThisDialog.cancel();
 
                         Toast.makeText(MainVer2Activity.this, "Data berhasil disimpan",Toast.LENGTH_SHORT).show();
 
-//                        finish();
-//                        return;
+
                     }
                 });
 
-                mBuilder.setView(mView);
-                AlertDialog dialog = mBuilder.create();
-                dialog.show();
+
+//        txtIdPuskesmas = (TextView) findViewById(R.id.txt_idPuskesmas);
+//        txtIdPuskesmas.setText("12121121");
+//
+//        txtNamaPuskesmas = (TextView) findViewById(R.id.txt_namaPuskesmas);
+//        txtNamaPuskesmas.setText("Sarijadi");
+
+//        ImageButton settingButton = (ImageButton) findViewById(R.id.btnSetting);
+//        settingButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainVer2Activity.this);
+//                View mView = getLayoutInflater().inflate(R.layout.dialog_puskesmas,null);
+//
+//                setConfig = new SetConfigVer2(getApplicationContext() );
+//                idPuskesmas = (EditText) findViewById(R.id.idPuskesmas);
+//                namaPuskesmas= (EditText) findViewById(R.id.namaPuskesmas);
+//                simpan = (Button) mView.findViewById(R.id.btnSimpan);
+//
+//                HashMap<String, String> setTexting = setConfig.getDetail();
+//                idPuskesmas.setText(setTexting.get(setConfig.KEY_IDPUSKES));
+//                namaPuskesmas.setText(setTexting.get(setConfig.KEY_NAMAPUSKES));
+//
+//                simpan.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view){
+////                        setConfig.createSetConfig(
+////                                idPuskesmas.getText().toString(),
+////                                namaPuskesmas.getText().toString()
+////                        );
+//
+//                        Toast.makeText(MainVer2Activity.this, "Data berhasil disimpan",Toast.LENGTH_SHORT).show();
+//
+////                        finish();
+////                        return;
+//                    }
+//                });
+//
+//                mBuilder.setView(mView);
+//                AlertDialog dialog = mBuilder.create();
+//                dialog.show();
+                ThisDialog.show();
 
             }
         });
-
-//        HashMap<String, String> setTexting = setConfig.getDetail();
-//        idPuskesmas.setText(setTexting.get(setConfig.KEY_IDPUSKES));
-//        namaPuskesmas.setText(setTexting.get(setConfig.KEY_NAMAPUSKES));
 
 //        if (setting.get(SetConfig.KEY_IDPUSKES) !=null){
 //            simpan.setText("UPDATE");
@@ -107,9 +139,25 @@ public class MainVer2Activity extends AppCompatActivity {
 //        });
 
         //getHPCdata();
+//    }
+
+        Show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences SimpanID = getApplicationContext().getSharedPreferences("IDPUSKESMAS", 0);
+                IDPuskesmas.setText(SimpanID.getString("IDPuskesmas", null));
+
+
+            }
+        });
     }
 
-
+    public void SharedPrefesSimpan(String IdPuskesmas){
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("IDPUSKESMAS", 0);
+        SharedPreferences.Editor prefEDIT = prefs.edit();
+        prefEDIT.putString("IDPuskesmas", IdPuskesmas);
+        prefEDIT.commit();
+    }
 
     public void goToPin(View v){
         Intent activity = new Intent(this, Pin2Activity.class);
@@ -133,79 +181,16 @@ public class MainVer2Activity extends AppCompatActivity {
             throw mSQLException;
         }
         String SQL_CREATE_KARTU_TABLE =  "CREATE TABLE " + EhealthContract.KartuEntry.TABLE_NAME + " ("
-                + EhealthContract.KartuEntry.COLUMN_HPCNUMBER + " TEXT, "
-                + EhealthContract.KartuEntry.COLUMN_DOKTER + " TEXT, "
-                + EhealthContract.KartuEntry.COLUMN_PDCNUMBER + " TEXT, "
-                + EhealthContract.KartuEntry.COLUMN_NAMAPASIEN + " TEXT);";
+                + EhealthContract.KartuEntry.COLUMN_HPCNUMBER + " TEXT NOT NULL, "
+                + EhealthContract.KartuEntry.COLUMN_DOKTER + " TEXT NOT NULL, "
+                + EhealthContract.KartuEntry.COLUMN_PDCNUMBER + "TEXT NOT NULL, "
+                + EhealthContract.KartuEntry.COLUMN_NAMAPASIEN + "TEXT NOT NULL);";
 
         // Execute the SQL statement
         SQLiteDatabase mDb = mDBHelper.getWritableDatabase();
         mDb.execSQL(SQL_CREATE_KARTU_TABLE);
-        mDBHelper.close();
     }
 
-    /** mengambil data dari kartu HPC
-     *
-     */
-
-    public void getHPCdata() {
-        // Read from input fields
-        // Use trim to eliminate leading or trailing white space
-        Boolean statusKartuHPC = true;
-        String HPCnumberString = "D12345";
-        String namaDokterString = "dr. Galuh";
-
-        if (statusKartuHPC != false) {
-
-            //Toast.makeText(this, "true ", Toast.LENGTH_SHORT).show();
-            // Create database helper
-            DatabaseHelper mDbHelper = new DatabaseHelper(getApplicationContext());
-            mDbHelper.openDataBase();
-            //mDbHelper.deleteAll();
-            // Gets the database in write mode
-            SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-            // Create a ContentValues object where column names are the keys
-            ContentValues values = new ContentValues();
-            values.put(EhealthContract.KartuEntry.COLUMN_HPCNUMBER, HPCnumberString);
-//            values.put(KartuEntry.COLUMN_PIN_HPC, PIN_HPC);
-            values.put(EhealthContract.KartuEntry.COLUMN_DOKTER, namaDokterString);
-
-            // Insert a new row in the database, returning the ID of that new row.
-            long newRowId = db.insert(EhealthContract.KartuEntry.TABLE_NAME, null, values);
-            mDbHelper.close();
-            // Show a toast message depending on whether or not the insertion was successful
-            if (newRowId == -1) {
-                // If the row ID is -1, then there was an error with insertion.
-                Toast.makeText(this, "Sinkronisasi kartu HPC GAGAL!", Toast.LENGTH_SHORT).show();
-            } else {
-                // Otherwise, the insertion was successful and we can display a toast with the row ID.
-                Toast.makeText(this, "Sinkronisasi kartu HPC BERHASIL! ", Toast.LENGTH_SHORT).show();
-//                Intent activity = new Intent(this, Pin2Activity.class);
-//                startActivity(activity);
-//                finish();
-            }
-        } else
-        {
-
-            AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainVer2Activity.this);
-            mBuilder.setIcon(R.drawable.logo2);
-            mBuilder.setTitle("Kartu yang Anda masukkan tidak dapat diakses");
-            mBuilder.setMessage("Silahkan coba lagi atau masukkan kartu lain");
-            mBuilder.setCancelable(false);
-            mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.dismiss();
-                }
-            });
-
-            AlertDialog alertDialog = mBuilder.create();
-            alertDialog.show();
-
-        }
-
-    }
 
 }
 
