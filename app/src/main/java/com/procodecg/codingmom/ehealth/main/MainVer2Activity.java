967eaf0a35f6fde0c678919f35b0be8f3b0df7da
Irma Hariawang <irma_loh@yahoo.com>
2017-11-14 15:42:36 +0700

    update MainVer2Activity nama dokter

--- app/src/main/java/com/procodecg/codingmom/ehealth/main/MainVer2Activity.java
+++ app/src/main/java/com/procodecg/codingmom/ehealth/main/MainVer2Activity.java
@@ -1,11 +1,14 @@
 package com.procodecg.codingmom.ehealth.main;
 
 import android.app.Dialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +44,7 @@ public class MainVer2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main_ver2);
 
         copyDBEhealth();
+        getHPCdata();
 
         DialogPuskesmas = (ImageButton) findViewById(R.id.btnSetting);
         Show = (Button) findViewById(R.id.btnShow);
@@ -181,16 +185,80 @@ public class MainVer2Activity extends AppCompatActivity {
             throw mSQLException;
         }
         String SQL_CREATE_KARTU_TABLE =  "CREATE TABLE " + EhealthContract.KartuEntry.TABLE_NAME + " ("
-                + EhealthContract.KartuEntry.COLUMN_HPCNUMBER + " TEXT NOT NULL, "
-                + EhealthContract.KartuEntry.COLUMN_DOKTER + " TEXT NOT NULL, "
-                + EhealthContract.KartuEntry.COLUMN_PDCNUMBER + "TEXT NOT NULL, "
-                + EhealthContract.KartuEntry.COLUMN_NAMAPASIEN + "TEXT NOT NULL);";
+                + EhealthContract.KartuEntry.COLUMN_HPCNUMBER + " TEXT, "
+                + EhealthContract.KartuEntry.COLUMN_DOKTER + " TEXT, "
+                + EhealthContract.KartuEntry.COLUMN_PDCNUMBER + " TEXT, "
+                + EhealthContract.KartuEntry.COLUMN_NAMAPASIEN + " TEXT);";
 
         // Execute the SQL statement
         SQLiteDatabase mDb = mDBHelper.getWritableDatabase();
         mDb.execSQL(SQL_CREATE_KARTU_TABLE);
+        mDBHelper.close();
     }
 
+
+    /** mengambil data dari kartu HPC
+     *
+     */
+
+    public void getHPCdata() {
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        Boolean statusKartuHPC = true;
+        String HPCnumberString = "D12345";
+        String namaDokterString = "dr. Sinta";
+
+        if (statusKartuHPC != false) {
+
+            //Toast.makeText(this, "true ", Toast.LENGTH_SHORT).show();
+            // Create database helper
+            DatabaseHelper mDbHelper = new DatabaseHelper(getApplicationContext());
+            mDbHelper.openDataBase();
+            //mDbHelper.deleteAll();
+            // Gets the database in write mode
+            SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+            // Create a ContentValues object where column names are the keys
+            ContentValues values = new ContentValues();
+            values.put(EhealthContract.KartuEntry.COLUMN_HPCNUMBER, HPCnumberString);
+//            values.put(KartuEntry.COLUMN_PIN_HPC, PIN_HPC);
+            values.put(EhealthContract.KartuEntry.COLUMN_DOKTER, namaDokterString);
+
+            // Insert a new row in the database, returning the ID of that new row.
+            long newRowId = db.insert(EhealthContract.KartuEntry.TABLE_NAME, null, values);
+            mDbHelper.close();
+            // Show a toast message depending on whether or not the insertion was successful
+            if (newRowId == -1) {
+                // If the row ID is -1, then there was an error with insertion.
+                Toast.makeText(this, "Sinkronisasi kartu HPC GAGAL!", Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the insertion was successful and we can display a toast with the row ID.
+                Toast.makeText(this, "Sinkronisasi kartu HPC BERHASIL! ", Toast.LENGTH_SHORT).show();
+//                Intent activity = new Intent(this, Pin2Activity.class);
+//                startActivity(activity);
+//                finish();
+            }
+        } else
+        {
+
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainVer2Activity.this);
+            mBuilder.setIcon(R.drawable.logo2);
+            mBuilder.setTitle("Kartu yang Anda masukkan tidak dapat diakses");
+            mBuilder.setMessage("Silahkan coba lagi atau masukkan kartu lain");
+            mBuilder.setCancelable(false);
+            mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+
+            AlertDialog alertDialog = mBuilder.create();
+            alertDialog.show();
+
+        }
+
+    }
 
 }
 
